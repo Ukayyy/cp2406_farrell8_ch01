313b5388a2c893859a441ac13950acf67c51cf7e
Ukayyy <38536436+Ukayyy@users.noreply.github.com>
2018-07-10 15:45:12 +0800

    Add files via upload

---
+++ Prac_1/MovieQuote06.java
@@ -0,0 +1,10 @@
+public class MovieQuote06
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Iron Man");
+        System.out.println("The Incredible Hulk");
+        System.out.println("Marvel's The Avengers");
+    }
+}
+
